3fa7a86686cdefe6524d298dcc0dca897c3de698
Jayden Huang <jahuang2008@gmail.com>
2025-10-30 20:24:18 -0700

    M7L5

---
+++ M07.telling_a_story_workbook/lessons/dialogue.gd
@@ -0,0 +1,28 @@
+extends Control
+
+@onready var rich_text_label: RichTextLabel = %RichTextLabel
+@onready var next_button: Button = %NextButton
+
+var dialogue_items: Array[String] = [
+	"Hello, I'm Jayden's Assistant!",
+	"My Name Is KoolPig",
+	"How May I Be Of Assistance To You Today?",
+	"No Problem, Sit Tight",
+]
+
+var current_item_index := 0
+
+func _ready() -> void:
+	show_text()
+	next_button.pressed.connect(advance)
+
+func show_text() -> void:
+	var current_item := dialogue_items[current_item_index]
+	rich_text_label.text = current_item
+
+func advance() -> void:
+	current_item_index += 1
+	if current_item_index == dialogue_items.size():
+		get_tree().quit()
+	else:
+		show_text()
